220eee17d3b491410bd2ff85a8f25c71c1210017
23Mubasshir <23mubasshir@gmail.com>
2023-02-18 21:02:11 +0600

    js and css added

--- js/calculation.js
+++ js/calculation.js
@@ -1,6 +1,36 @@
 
 
+// ------Serial Number---- 
+let serial = 0
 
+
+// ------ Triangle ----- 
+    document.getElementById('btn-triangle').addEventListener('click', function(){
+    
+    
+    const nameTriangle =document.getElementById("name-triangle").innerText;
+    const depositFieldTriangleB = document.getElementById('triangle-b');
+    const triangleBString = depositFieldTriangleB.value;
+    const triangleB = parseFloat(triangleBString);
+
+    const depositFieldTriangleH = document.getElementById('triangle-h');
+    const triangleHString = depositFieldTriangleH.value;
+    const triangleH = parseFloat(triangleHString);
+
+    depositFieldTriangleB.value = '';
+    depositFieldTriangleH.value = '';
+    //----- function-1-----
+    giveData(triangleB, triangleH);
+
+    serial +=1;
+
+    const triangleArea = 0.5 * triangleB * triangleH ;
+    const triangleAreaFixed = triangleArea.toFixed(2);
+    //----- function-2-----
+    displayData(nameTriangle, triangleAreaFixed);
+    
+
+    })
 
 
 
