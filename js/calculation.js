f53cd91bfa31c382d16a00ac3ebd217ebdcac23f
23Mubasshir <23mubasshir@gmail.com>
2023-02-18 21:11:19 +0600

    js added

--- js/calculation.js
+++ js/calculation.js
@@ -34,11 +34,65 @@ let serial = 0
 
 
 
+// ------ Rectangle ----- 
+
+document.getElementById('dtn-rectangle').addEventListener('click', function(){
+
+
+    const nameRectangle =document.getElementById("name-rectangle").innerText;
+    const depositFieldRectangleW =document.getElementById('rectangle-w');
+    const rectangleWString = depositFieldRectangleW.value;
+    const rectangleW = parseFloat(rectangleWString);
+
+    const depositFieldRectangleL =document.getElementById('rectangle-l');
+    const rectangleLString = depositFieldRectangleL.value;
+    const rectangleL = parseFloat(rectangleLString);
+
+    depositFieldRectangleW.value = '';
+    depositFieldRectangleL.value = '';
+    //----- function-1-----
+    giveData(rectangleL, rectangleW);
+
+    serial +=1;
+
+    const rectangleArea = rectangleW * rectangleL;
+    const rectangleAreaFixed = rectangleArea.toFixed(2);
+    //----- function-2-----
+    displayData(nameRectangle, rectangleAreaFixed);
+
+
+})
 
 
 
+// ------ Parallelogram ----- 
+
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+
+    
+    const nameParallelogram =document.getElementById("name-parallelogram").innerText;
+
+    const depositFieldParallelogramB = document.getElementById('parallelogram-b');
+    const parallelogramBString = depositFieldParallelogramB.value;
+    const parallelogramB = parseFloat(parallelogramBString);
+
+    const depositFieldParallelogramH = document.getElementById('parallelogram-h');
+    const parallelogramHString = depositFieldParallelogramH.value;
+    const parallelogramH = parseFloat(parallelogramHString);
+
+    depositFieldParallelogramB.value = '';
+    depositFieldParallelogramH.value = '';
+    //----- function-1-----
+    giveData(parallelogramB, parallelogramH);
+    serial +=1;
+
+    const parallelogramArea =  parallelogramB * parallelogramH ;
+    const parallelogramAreaFixed = parallelogramArea.toFixed(2);
+    //----- function-2-----
+    displayData(nameParallelogram, parallelogramAreaFixed);
 
 
+})
 
 
 
