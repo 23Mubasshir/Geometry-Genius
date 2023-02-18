4fe6c1318f0b577003de8e141a4c146a16e3599e
23Mubasshir <23mubasshir@gmail.com>
2023-02-18 21:49:06 +0600

    color change added

--- js/color.js
+++ js/color.js
@@ -1,4 +1,31 @@
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
 
+
+  document.getElementById('card-body-color').addEventListener("mouseover", function(){
+    document.getElementById('card-body-color').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card-body-color1').addEventListener("mouseover", function(){
+    document.getElementById('card-body-color1').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card-body-color2').addEventListener("mouseover", function(){
+    document.getElementById('card-body-color2').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card-body-color3').addEventListener("mouseover", function(){
+    document.getElementById('card-body-color3').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card-body-color4').addEventListener("mouseover", function(){
+    document.getElementById('card-body-color4').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card-body-color5').addEventListener("mouseover", function(){
+    document.getElementById('card-body-color5').style.backgroundColor = randomColor();
+  });
 
 
 
